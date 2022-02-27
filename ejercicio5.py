92a08d7dce07ff36804c16d82d1c6b7340461e21
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-02-27 15:54:19 +0000

    creo la variable m, que es el valor medio

--- ejercicio5.py
+++ ejercicio5.py
@@ -2,15 +2,20 @@ tabla = [1,2,3,4,5,6,7,8]
 n = int(input('Introduzca el valor que desea encontrar:'))
 i = 0
 j = len(tabla)
+m = (i+j)/2
 
 class tabl@:
-  def __init__(self,tabla,n):
+  def __init__(self,tabla,n,i,j,m):
     self.i= i
     self.j= j
+    self.n=
 
     if self.i > self.j:
-      return'No hay ningún valor en la posición selccionada')
+      return'No hay ningún valor en la posición seleccionada'
+      
     else:
+      self.m = m
+      
       
 
   
