4522d4f156e1f51b6a648a3ffb72c220076654e2
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-02-27 15:31:48 +0000

    creamos la función y añadimos los valores que nos faltaban

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,3 +1,13 @@
 tabla = [1,2,3,4,5,6,7,8]
 n = int(input('Introduzca el valor que desea encontrar:'))
+i = 0
+j = len(tabla)
 
+class tabl@:
+  def __init__(self,tabla,n):
+    self.i= i
+    self.j= j
+
+  
+    #definimos columnas y filas
+   
