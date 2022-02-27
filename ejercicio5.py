596ec19de1d9270eed3178d8a665f82e3a5fb0e4
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-02-27 16:14:45 +0000

    hemos terminado de definir todos las condiciones

--- ejercicio5.py
+++ ejercicio5.py
@@ -5,16 +5,30 @@ j = len(tabla)
 m = (i+j)/2
 
 class tabl@:
-  def __init__(self,tabla,n,i,j,m):
+  def  __init__(self,tabla,n,i,j):
     self.i= i
     self.j= j
-    self.n=
+    self.n= n
+    
+  def busqueda_numero (self):
 
     if self.i > self.j:
       return'No hay ningún valor en la posición seleccionada'
       
     else:
       self.m = m
+      if tabla[self.m] == n:
+        return self.m
+      elif tabla[self.m] > n:
+        print (busqueda_numero(self))
+        return busqueda_numero (self,self.m -1)
+      else:
+        return busqueda_numero (self, self.m + 1)
+
+  
+        
+        
+        
       
       
 
