fa8d66efc343f0097ebb3c389f83d2f6f668d291
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-02-27 22:22:12 +0000

    mejoro el main

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import ejercicio5
-import ejercicio6
-import ejercicio7
-
+variable = int(input("Seleccione que ejercicio desea ejecutar(5-7): "))
+if variable == 5:
+    import ejercicio5
+elif variable == 6:
+    import ejercicio6
+elif variable == 7:
+    import ejercicio7
+else:
+    print("Introduzca valores entre 5 y 7.")
