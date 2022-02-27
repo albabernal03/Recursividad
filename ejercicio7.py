f10761e687ac885687782c38c4be5bc0a4a8d766
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-02-27 21:30:59 +0000

    hay un fallo pero ns cual es

--- ejercicio7.py
+++ ejercicio7.py
@@ -12,4 +12,24 @@ class organizacion:
     self.rojo=rojo
     self.verde=verde
     self.azul=azul
-  def reorganizar(self):
+   def reorganizar_colores(self):
+    if len (self.bandera) > 0:
+      color = self.bandera.pop(0)
+
+    if color == "R":
+      self.rojo.append(color)
+      return self.bandera
+    if color == "A":
+      self.azul.append(color)
+      return self.bandera
+    if color == "V":
+      self.verde.append(color)
+      return self.bandera
+    else:
+        resultado_bandera=rojo+verde+azul
+        print('La bandera ordenada es la siguiente: {resultado_bandera}')
+
+resultado=organizacion(bandera,rojo,verde,azul)
+print(resultado.reorganizar_colores)
+    
+    
