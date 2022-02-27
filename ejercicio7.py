debfa8f9c3b671adfd9450c283770f077427af40
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-02-27 21:16:50 +0000

    creamos la funcion __init__

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,4 +1,15 @@
 #R= ROJO
 #V=VERDE
 #A=AZUL
-bandera = ["R","A","A","R","V","A","A","R","A","R","R","V","R","R","A","V","V"]
+bandera = ["R","A","A","R","V","A","A","R","A","R","R","V","R","R","A","V","V"] #El orden ya viene dado por el ejercicio
+rojo = []
+verde=[]
+azul=[]
+
+class organizacion:
+  def __init__(self,bandera,rojo,verde,azul):
+    self.bandera=bandera
+    self.rojo=rojo
+    self.verde=verde
+    self.azul=azul
+  def reorganizar(self):
